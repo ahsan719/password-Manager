9ce152650297bdf2ec590e2bb22f7bbda69b1d79
Ahsan Naseer <ahsannilore@gmail.com>
2024-07-15 20:40:36 +0500

    UI Creation

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, {useState , useRef}from 'react'
 
 function Manager() {
+ 
+    const [passData, setpassData] = useState({})
+
+    const addData =() => { 
+        console.log(passData)
+    }
+    const handleChange= (e) => { 
+        setpassData({...passData , [e.target.name]: e.target.value})
+     }
+
   return (
     <>
    
 
-        <div className=" bg-slate-800 max-w-4xl mx-auto">
-            <h1>PassMan</h1>
-            <p>Your Own password Manager</p>
+        <div className="  mx-auto max-w-5xl mycontainer ">
 
-        <div className="text-white flex flex-col p-4">
-            <input type="text"  />
-            <div className="rounded-full">
+            <h1 className='text-4xl text-center font-bold'> 
+                <span className="text-green-500">&lt;</span>
+                   Pass 
+                <span className="text-green-500">OP/&gt; </span>
+             </h1>
 
-            <input type="text" />
-            <input type="text" />
+            <p className='text-green-600 text-lg text-center p-2'>Your Own password Manager</p>
+
+        <div className=" flex flex-col p-4 gap-3 items-center">
+            <input  className="rounded-full border border-green-500 w-full p-4 py-1 " type="text"name="url" placeholder='Enter Website URL' onChange={(e)=> handleChange(e)} /> 
+            <div className="flex w-full gap-7" >
+
+            <input  className="rounded-full border border-green-500 w-full p-4 py-1 " type="text" name="username" placeholder='Enter UserName'onChange={(e)=> handleChange(e)} /> 
+            <input  className="rounded-full border border-green-500 w-full p-4 py-1 " type="text" name="password" placeholder='Enter Password'onChange={(e)=> handleChange(e)}/> 
             </div>
+            <button onClick={()=> addData()} className='bg-green-500  rounded-full px-4 py-1 w-fit hover:bg-green-400 mt-4'>Add Password</button>
         </div>
         </div>
       
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 
 function Navbar() {
   return (
-   <nav className='bg-purple-200 flex justify-between
-    items-center px-4 h-11 '>
-    <div className="logo font-bold">PassMan</div>
+   <nav className='bg-slate-800 text-white '>
+        <div className="mycontainer flex justify-between mycontainer
+    items-center px-4 h-11 py-7">
+
+    <div className="logo font-bold text-white text-xl" >  
+        <span className="text-green-500">&lt;</span>
+        Pass 
+        <span className="text-green-500">OP/&gt; </span> 
+
+
+    </div>
     <ul className='flex gap-4 ' >
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
         <li><a href="/logout">Logout</a></li>
      </ul>
+        </div>
    </nav>
   )
 }
