2432221edc15aea65fa8f05df8526f535700bd8e
Ahsan Naseer <ahsannilore@gmail.com>
2024-07-29 18:42:10 +0500

    sticky fixed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
     <>
-  <nav className='bg-slate-800 bottom-0  w-full py-2 text-white '>
-    <p className='ml-3'>Developed by Ahsan Naseer</p> 
-     </nav>
-    
+      <nav className="bg-slate-800 bottom-0 w-full sticky text-white md:text-lg lg:text-xl">
+        <p className="ml-3 md:ml-5 lg:ml-10">Developed by Ahsan Naseer</p>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Footer 
+export default Footer;
