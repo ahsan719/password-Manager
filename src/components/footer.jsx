e859f31a6bd658d402d11ffc9861f48a34a06e16
Ahsan Naseer <ahsannilore@gmail.com>
2024-07-29 18:26:13 +0500

    footer added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 function Footer() {
   return (
     <>
-  <nav className='py-2 mx-auto w-full bg-slate-800'>
-     Developeded by Ahsan Naseer</nav>
+  <nav className='bg-slate-800 bottom-0  w-full py-2 text-white '>
+    <p className='ml-3'>Developed by Ahsan Naseer</p> 
+     </nav>
     
     </>
   )
 }
 
-export default Footer
+export default Footer 
